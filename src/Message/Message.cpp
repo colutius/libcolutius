9a571ea7472152e786892f8192f5419d6624ab7f
ambition_echo <ambition_echo@outlook.com>
2022-05-09 21:15:46 +0800

    添加Doxyfile

--- src/Message/Message.cpp
+++ src/Message/Message.cpp
@@ -1,13 +1,26 @@
+/**
+ * @file Message.cpp
+ * @author ambition_echo (ambition_echo@outlook.com)
+ * @brief IRC消息类实现
+ * @version 0.1
+ * @date 2022-05-09
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include "Message.h"
 
 Message::Message(QObject *parent) : QObject(parent)
 {
 }
 Message::~Message() = default;
-/*!
- * @brief 测试函数
+/**
+ * @brief 自动文档生成测试函数
+ *        发送消息
+ * 
+ * @param msg 
  */
-void Message::sendMsg()
+void Message::sendMsg(QString msg)
 {
-    qDebug() << "hello shared library";
+    qDebug() << "hello shared library"+msg;
 }
--- src/Message/Message.h
+++ src/Message/Message.h
@@ -1,12 +1,25 @@
+/**
+ * @file Message.h
+ * @author ambition_echo (ambition_echo@outlook.com)
+ * @brief  IRC消息类头文件
+ * @version 0.1
+ * @date 2022-05-09
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #pragma once
 #include <QDebug>
 #include <QObject>
-
+/**
+ * @brief IRC消息类
+ * 
+ */
 class Message : public QObject
 {
     Q_OBJECT
   public:
     Message(QObject *parent = nullptr);
     ~Message() override;
-    static void sendMsg();
+    static void sendMsg(QString msg);
 };
