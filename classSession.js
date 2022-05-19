f6e26a1bf200eda143607c52921efff4bd133d8a
ambition_echo <ambition_echo@outlook.com>
2022-05-19 02:44:38 +0000

    update Docs 7d06e00931fd0fd861b13f4a071964dde80d5178

--- classSession.js
+++ classSession.js
@@ -3,7 +3,11 @@ var classSession =
     [ "Session", "classSession.html#a19e2459b4216f4092ace8907d18cea11", null ],
     [ "~Session", "classSession.html#a0f31ff6c59ea04a520655cbc11cf63bc", null ],
     [ "addMessage", "classSession.html#afb689e2f0273d2b8fb70c93f8a443385", null ],
+    [ "getMessage", "classSession.html#a4bcfdaa8235f6d8352b3cfb87a2349be", null ],
+    [ "getMessageList", "classSession.html#addf80877b3d4cdd3a2f7188b5639c37e", null ],
+    [ "getMessageNum", "classSession.html#a23796f63fcbc7d7fbe8b162137594a8c", null ],
     [ "getName", "classSession.html#a1cd7ee30ac0e1ac24b3c338cd6ba1fa8", null ],
+    [ "getType", "classSession.html#a3913846b042c3d42123623c4bf9c5808", null ],
     [ "newMsg", "classSession.html#a7ef26b4c9ded1883f03aa5e3394d3709", null ],
     [ "_messageList", "classSession.html#a229d87304936774e9d26d16da4d11a1e", null ],
     [ "_name", "classSession.html#a4f13755efa180e3a475a9a5f1a252fda", null ]
